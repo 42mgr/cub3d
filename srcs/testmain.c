c10b68832835202e203eec20c307431b988c2c04
Marcel <mgraf@student.42berlin.de>
2023-09-19 16:06:53 +0200

    setup to start coding seems to be ready

--- srcs/testmain.c
+++ srcs/testmain.c
@@ -1,15 +1,16 @@
 #include <sys/types.h>
 #include <stddef.h>
+#include "cub3d.h"
 
 //typedef int mlx_t;
 //typedef int bool;
 
-void	ft_putstr_fd(char *s, int fd);
+
 //static bool mlx_init_render(mlx_t* mlx);
 
 int main()
 {
-	ft_putstr_fd("hello", 1);
+	ft_putstr_fd("hello1551", 1);
 //	mlx_init_render(NULL);
 	return (0);
 }
