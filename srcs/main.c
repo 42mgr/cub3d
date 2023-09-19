bebc2cd166091ee4287104a8a44bf8bf71d20681
Marcel <mgraf@student.42berlin.de>
2023-09-19 16:28:39 +0200

    added maps

---
+++ srcs/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgraf <mgraf@student.42berlin.de>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 16:09:28 by mgraf             #+#    #+#             */
+/*   Updated: 2023/09/19 16:15:14 by mgraf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int	main(int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+	(void)env;
+
+	// map parser and validation
+
+	// raycaster (mini-map) 2D
+
+	// fake 3D
+
+	return (0);
+}
